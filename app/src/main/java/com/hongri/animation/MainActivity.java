f9d5caef26256d3d29f74e6c0c9fed545f769be1
zhongyao <zhongyao@alibaba-inc.com>
2018-07-25 17:32:07 +0800

    init

--- app/src/main/java/com/hongri/animation/MainActivity.java
+++ app/src/main/java/com/hongri/animation/MainActivity.java
@@ -3,6 +3,9 @@ package com.hongri.animation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author 红日
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
